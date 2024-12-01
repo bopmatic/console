2a446d4b01a13f9697f99a14bae83c86b635b569
Chris Arensdorf <carensdorf@cloudscionconsulting.com>
2024-12-01 11:39:08 -0500

    Fixes #17: Add retry logic for axios client

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,3 +1,4 @@
+import axios, { AxiosRequestConfig, AxiosError, AxiosInstance } from 'axios';
 import { Configuration } from './configuration';
 import { BASE_PATH } from './base';
 import { ServiceRunnerApi } from './api';
@@ -9,7 +10,7 @@ export const updateBopmaticClientToken = (newToken: string) => {
   token = newToken;
   // Reinitialize configuration with the new token
   configuration.apiKey = `Bearer ${token}`;
-  client = new ServiceRunnerApi(configuration);
+  client = createServiceRunnerApiInstance(configuration);
 };
 
 const configuration = new Configuration({
@@ -17,13 +18,46 @@ const configuration = new Configuration({
   apiKey: `Bearer ${token}`,
 });
 
-let client = new ServiceRunnerApi(configuration);
+// Initialize Axios instance
+const axiosInstance: AxiosInstance = axios.create();
+
+// Configure retry logic for Axios
+axiosInstance.interceptors.response.use(
+  (response) => response, // Pass through successful responses
+  async (error: AxiosError) => {
+    const config = error.config as AxiosRequestConfig & { retryCount?: number };
+
+    // Retry logic for HTTP 429
+    // TODO: Remove the 'OR' section of this IF statement once we figure out the CORS exceptions that come back from API
+    if (error.response?.status === 429 || error.code === 'ERR_NETWORK') {
+      const retryAfter =
+        parseInt(error.response?.headers['retry-after'], 10) || 1; // Default retry after 1 second
+      config.retryCount = (config.retryCount || 0) + 1;
+
+      if (config.retryCount <= 5) {
+        await new Promise((resolve) => setTimeout(resolve, retryAfter * 1000)); // Wait before retrying
+        return axiosInstance(config); // Retry the request
+      }
+    }
+
+    return Promise.reject(error); // Reject if not retryable or exceeded retry limit
+  }
+);
+
+// Create ServiceRunnerApi instance
+let client = createServiceRunnerApiInstance(configuration);
+
+function createServiceRunnerApiInstance(
+  configuration: Configuration
+): ServiceRunnerApi {
+  return new ServiceRunnerApi(configuration, BASE_PATH, axiosInstance);
+}
+
+const signupClient = new SignUpApi();
 
 export const getBopmaticClient = () => {
   return client;
 };
-
-const signupClient = new SignUpApi();
 
 export const getSignupClient = () => {
   return signupClient;
